1e8d174cb4877340f3c44887d3ce826481bd137a
chathushkax <k1879.nayanajith@gmail.com>
2024-06-20 16:40:12 +0530

    showtime page modifed

--- database/migrations/2024_06_15_094209_create_movies_table.php
+++ database/migrations/2024_06_15_094209_create_movies_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->string('genre');
+            $table->string('image_url');
             $table->date('release_date');
             $table->timestamps();
         });
